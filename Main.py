80b38dae259fa4b140c8bd490762330b0787b026
Alcomendras26 <vincent.eerst.alcomendras@stu.ocsb.ca>
2026-09-21 11:13:40 -0400

    Setting up

--- Main.py
+++ Main.py
@@ -1,10 +1,22 @@
 #############################################
-# Name: Your name
+# Name: Vincent Alcomendras
 # Class: ICS3C
 # Date: Friday Sept. 25
 # Project Name: Week4Tournament
 #
 # Project Description: See the README file
 #############################################
+from asyncio import FrameCallGraphEntry
 
 # THIS IS WHERE YOU CODE
+
+print("Welcome to the tournament ranking program!")
+
+print("Enter the team name")
+
+team1=input()
+team2=input()
+team3=input()
+team4=input()
+team5=input()
+team6=input()
