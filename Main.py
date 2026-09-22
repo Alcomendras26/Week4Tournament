f6b87c8e2450bf4838966586c7a500ce74f876a5
Alcomendras26 <vincent.eerst.alcomendras@stu.ocsb.ca>
2026-09-22 11:15:41 -0400

    Date2

--- Main.py
+++ Main.py
@@ -12,11 +12,54 @@ from asyncio import FrameCallGraphEntry
 
 print("Welcome to the tournament ranking program!")
 
-print("Enter the team name")
 
+name1=input()
 team1=input()
+wins1=int(input("How many wins for") , name1 , ("?")
+ties1=int(input("How many ties for South Korea?"))
+
+print("The total is")
+var1=input (wins1 * 2 + ties1)
+
 team2=input()
+wins2=int(input("How many wins for Malaysia?"))
+ties2=int(input("How many ties for Malaysia?"))
+
+print("The total is")
+var2=input (wins2 * 2 + ties2)
+
 team3=input()
+wins3=int(input("How many wins for China?"))
+ties3=int(input("How many ties for China?"))
+
+print("The total is")
+var3=input (wins3 * 2 + ties3)
+
 team4=input()
+wins4=int(input("How many wins for Indonesia?"))
+ties4=int(input("How many ties for Indonesia?"))
+
+print("The total is")
+var4=input (wins4 * 2 + ties4)
+
 team5=input()
+wins5=int(input("How many wins for Singapore?"))
+ties5=int(input("How many ties for Singapore?"))
+
+print("The total is")
+var5=input (wins5 * 2 + ties5)
+
 team6=input()
+wins6=int(input("How many wins for Japan?"))
+ties6=int(input("How many ties for Japan?"))
+
+print("The total is")
+var6=input (wins6 * 2 + ties6)
+
+if var1>var2 and var1>var3 and var1>var4 and var1>var5 and var1>var6:
+    print(team1)
+
+
+
+
+
