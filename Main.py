df93aa2fbddb51b7b8519d40b97167f6743b7bc9
Alcomendras26 <vincent.eerst.alcomendras@stu.ocsb.ca>
2026-09-23 11:12:57 -0400

    very good

--- Main.py
+++ Main.py
@@ -12,52 +12,90 @@ from asyncio import FrameCallGraphEntry
 
 print("Welcome to the tournament ranking program!")
 
-
-name1=input()
+print("Input team name")
 team1=input()
-wins1=int(input("How many wins for") , name1 , ("?")
-ties1=int(input("How many ties for South Korea?"))
+wins1=int(input(f"How many wins for {team1}?"))
+ties1=int(input(f"How many ties for {team1}?"))
+los1=int(input(f"How many loses for {team1}?"))
 
-print("The total is")
-var1=input (wins1 * 2 + ties1)
+var1=wins1 * 2 + ties1
+print(f"The total is {var1}")
 
+
+print("Input team name")
 team2=input()
-wins2=int(input("How many wins for Malaysia?"))
-ties2=int(input("How many ties for Malaysia?"))
+wins2=int(input(f"How many wins for {team2}?"))
+ties2=int(input(f"How many ties for {team2}?"))
+los2=int(input(f"How many loses for {team2}?"))
 
-print("The total is")
-var2=input (wins2 * 2 + ties2)
+var2=wins2 * 2 + ties2
+print(f"The total is {var2}")
 
+
+print("Input team name")
 team3=input()
-wins3=int(input("How many wins for China?"))
-ties3=int(input("How many ties for China?"))
+wins3=int(input(f"How many wins for {team3}?"))
+ties3=int(input(f"How many ties for {team3}?"))
+los3=int(input(f"How many loses for {team3}?"))
 
-print("The total is")
-var3=input (wins3 * 2 + ties3)
+var3=wins3 * 2 + ties3
+print(f"The total is {var3}")
 
+print("Input team name")
 team4=input()
-wins4=int(input("How many wins for Indonesia?"))
-ties4=int(input("How many ties for Indonesia?"))
+wins4=int(input(f"How many wins for {team4}?"))
+ties4=int(input(f"How many ties for {team4}?"))
+los4=int(input(f"How many loses for {team4}?"))
 
-print("The total is")
-var4=input (wins4 * 2 + ties4)
+var4=wins4 * 2 + ties4
+print(f"The total is {var4}")
 
+print("Input team name")
 team5=input()
-wins5=int(input("How many wins for Singapore?"))
-ties5=int(input("How many ties for Singapore?"))
+wins5=int(input(f"How many wins for {team5}?"))
+ties5=int(input(f"How many ties for {team5}?"))
+los5=int(input(f"How many loses for {team5}?"))
 
-print("The total is")
-var5=input (wins5 * 2 + ties5)
+var5=wins5 * 2 + ties5
+print(f"The total is {var5}")
 
+print("Input team name")
 team6=input()
-wins6=int(input("How many wins for Japan?"))
-ties6=int(input("How many ties for Japan?"))
+wins6=int(input(f"How many wins for {team6}?"))
+ties6=int(input(f"How many ties for {team6}?"))
+los6=int(input(f"How many loses for {team6}?"))
 
-print("The total is")
-var6=input (wins6 * 2 + ties6)
+var6=wins6 * 2 + ties6
+print(f"The total is {var6}")
+
 
 if var1>var2 and var1>var3 and var1>var4 and var1>var5 and var1>var6:
-    print(team1)
+    print(team1 , "was first place")
+
+if var2>var1 and var2>var3 and var2>var4 and var2>var5 and var2>var6:
+    print(team2 , "was first place")
+
+if var3>var1 and var3>var2 and var3>var4 and var3>var5 and var3>var6:
+    print(team3 , "was first place")
+
+if var4>var1 and var4>var2 and var4>var3 and var4>var5 and var4>var6:
+    print(team4 , "was first place")
+
+if var5>var1 and var5>var2 and var5>var3 and var5>var4 and var3>var6:
+    print(team5 , "was first place")
+
+if var6>var1 and var6>var2 and var6>var3 and var6>var4 and var6>var5:
+    print(team6 , "was first place")
+
+
+
+
+
+
+
+
+
+
 
 
 
